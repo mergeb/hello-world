d79f1cc953bd2a747dffe14de69008b6b33971cb
Meron Gosaye <meron.gosaye@pcc.edu>
2023-10-20 23:20:22 -0700

    added author name and version

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -2,6 +2,9 @@ package edu.pcc.cis.helloworld;
 
 import java.util.Date;
 
+/**
+ * Created by Meron Gosaye on 10/20/2023
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
